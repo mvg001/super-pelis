e98e2dd013db88483d94f80ac5844a2b897d3595
mvg001 <m.vassallo.giuliano@gmail.com>
2024-02-12 09:59:12 +0100

    ficha-peli actualizado

--- src/app/components/ficha-peli/ficha-peli.component.ts
+++ src/app/components/ficha-peli/ficha-peli.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ServicioPelisService } from '../../services/servicio-pelis.service';
+import { Peli } from '../../interfaces/peli';
 
 @Component({
   selector: 'app-ficha-peli',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './ficha-peli.component.html',
   styleUrl: './ficha-peli.component.scss'
 })
 export class FichaPeliComponent {
-  constructor(private ar:ActivatedRoute) {}
-  private titulo: string = '';
+  titulo: string = '';
+  peli: Peli | undefined;
+  constructor(private ar:ActivatedRoute, private svcPelis:ServicioPelisService) {
+  }
   ngOnInit():void {
     this.ar.params.subscribe(params => {
       this.titulo = params['titulo'];
     });
+    this.peli = this.svcPelis.buscarPeli(this.titulo);
   }
 }
--- src/app/services/servicio-pelis.service.ts
+++ src/app/services/servicio-pelis.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Peli } from '../interfaces/peli';
+const LOCAL_STORAGE_NAME = 'peliculas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioPelisService {
-  public pelis: Peli[] = [];
+
+  public pelis: Peli[];
   constructor() {
-    this.pelis = recuperarPeliculas();
+    this.pelis = recuperarPeliculas(LOCAL_STORAGE_NAME);
   }
   agregarPeli(nuevaPeli: Peli) {
     let index = this.pelis.push(nuevaPeli);
-    guardarPeliculas(this.pelis);
+    guardarPeliculas(LOCAL_STORAGE_NAME,this.pelis);
   }
   eliminarPeli(titulo: string) {
     let posicion = this.pelis.findIndex((p) => p.title === titulo);
     if (posicion >= 0) {
       this.pelis.splice(posicion,1);
-      guardarPeliculas(this.pelis);
+      guardarPeliculas(LOCAL_STORAGE_NAME,this.pelis);
     }
   }
   buscarPeli(titulo:string):Peli|undefined {
@@ -32,13 +34,12 @@ export class ServicioPelisService {
   }
 }
 
-const LOCAL_STORAGE_NAME = 'peliculas';
-function guardarPeliculas(pelis: Peli[]) {
-  localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(pelis));
+function guardarPeliculas(local_name:string, pelis: Peli[]) {
+  localStorage.setItem(local_name, JSON.stringify(pelis));
 }
 
-function recuperarPeliculas(): Peli[] {
-  let peliculasJSON = localStorage.getItem(LOCAL_STORAGE_NAME);
+function recuperarPeliculas(local_name:string): Peli[] {
+  let peliculasJSON = localStorage.getItem(local_name);
   if (peliculasJSON != null) {
     return JSON.parse(peliculasJSON);
   }
